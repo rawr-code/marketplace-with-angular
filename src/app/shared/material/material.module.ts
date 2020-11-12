76f511e271ddea524ef5630283429b9d1cee0cc8
Emmanuel Villegas Gonzalez <emmanuelvillegasgonzalez@gmail.com>
2020-11-11 21:59:49 -0400

    add model and update material module

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -4,10 +4,37 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatInputModule, MatButtonModule, MatCardModule],
-  exports: [MatInputModule, MatButtonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDialogModule,
+    MatProgressBarModule,
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatDialogModule,
+    MatProgressBarModule,
+  ],
 })
 export class MaterialModule {}
